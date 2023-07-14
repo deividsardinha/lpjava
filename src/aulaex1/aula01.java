d7065ec66e37dd6bac668b07be57282181b585ad
Deivid Sardinha <deividsardinha@gmail.com>
2023-07-14 16:44:35 -0300

    aula-java147

---
+++ src/aulaex1/aula01.java
@@ -0,0 +1,13 @@
+package aulaex1;
+/**
+ * Exercício 01
+ * Aula:10/07/2023
+ * Disciplina: Linguagem de Programação
+ * @author Deivid Sardinha
+ */
+public class aula01 {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+    }
+    
+}
