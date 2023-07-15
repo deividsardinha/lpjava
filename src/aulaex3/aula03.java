28dc1cfdab29b137bba05e492e79f580e9b926a1
deividsardinha <deividsardinha@gmail.com>
2023-07-15 09:45:49 -0300

    aula-java-github157

---
+++ src/aulaex3/aula03.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulaex3;
+
+/**
+ *
+ * @author SANDMAN
+ */
+public class aula03 {
+    public static void main(String[] args) {
+        System.out.println("Aula usando o github");
+    }
+}
